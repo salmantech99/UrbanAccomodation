8f54f0ad1f7340158bfd1f813e16c970fd5a52c2
M-jawad-PC\M-jawad <buttzohaib46@yahoo.com>
2018-07-06 15:08:52 -0700

    car model or backend car

---
+++ application/views/backend/cars/addcar.php
@@ -0,0 +1,37 @@
+            <!-- MAIN CONTENT-->
+            <div class="main-content" style="background-color: white;">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <form>
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <h5 class="addcar-status">Status</h5>
+                                </div>
+                                <div class="col-md-10">
+                                    <select class="addcar-status-work">
+                                        <option>Enabled</option>
+                                        <option>Disabled</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row" style="margin-top: 20px;">
+                                <div class="col-md-2">
+                                    <h5>Car Name</h5>
+                                </div>
+                                <div class="col-md-10">
+                                    <input type="text" name="" class="input-car-name" placeholder="Car Name">
+                                </div>
+                            </div>
+                            <div class="row" style="margin-top: 20px;">
+                                <div class="col-md-2">
+                                    <h5>Car Description</h5>
+                                </div>
+                                <div class="col-md-10">
+                                    <textarea name="" id="" cols="30" rows="6" class="input-car-name"></textarea>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div> 
+            <!-- END MAIN CONTENT-->
