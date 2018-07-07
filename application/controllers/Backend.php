5bb3164d556779cd229318241bc3f9b1ff5b3d48
Hasiba <hasiba.xpertcoders@gmail.com>
2018-07-07 00:44:23 -0700

    push

--- application/controllers/Backend.php
+++ application/controllers/Backend.php
@@ -6,13 +6,25 @@ class Backend extends CI_Controller {
 		public function overview(){	
 		$this->load->view('backend/template/header');
 		$this->load->view('backend/template/nav');
-		$this->load->view('backend/content/overview');
+		$this->load->view('backend/content/hotels/overview');
 		$this->load->view('backend/template/footer');
 	}
 		public function hotels(){	
 		$this->load->view('backend/template/header');
 		$this->load->view('backend/template/nav');
-		$this->load->view('backend/content/hotels');
+		$this->load->view('backend/content/hotels/hotels');
+		$this->load->view('backend/template/footer');
+	}
+		public function add_facilities(){	
+		$this->load->view('backend/template/header');
+		$this->load->view('backend/template/nav');
+		$this->load->view('backend/content/hotels/hotel-add');
+		$this->load->view('backend/template/footer');
+	}
+		public function cars(){	
+		$this->load->view('backend/template/header');
+		$this->load->view('backend/template/nav');
+		$this->load->view('backend/content/cars/addcar');
 		$this->load->view('backend/template/footer');
 	}
 	
