ad07dd63b823f4db53017770dc0836b5f5b0e3b6
M-jawad-PC\M-jawad <buttzohaib46@yahoo.com>
2018-07-17 18:00:34 -0700

    hello

--- application/views/backend/content/cars/cars_add.php
+++ application/views/backend/content/cars/cars_add.php
@@ -9,7 +9,7 @@
         <div class="tab-content">                                 
             <div id="home" class="tab-pane fade in active fade active show">
                 <div class="container-fluid">
-                    <form method="post" action="<?php echo base_url(); ?>backend/cars_add">
+                    <form method="post" action="<?php echo base_url(); ?>backend/insert_car">
                     
                         <div class="row">
                             <div class="col-md-2">
