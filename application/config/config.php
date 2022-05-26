b1795f30f5a3ee762c050f08c9bbb0db19af208d
Ahtisham Maqbool <ahtishammaqbool02@gmail.com>
2022-05-26 14:08:43 +0500

    commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://iahtisham.com/booking/';
+$config['base_url'] = 'http://localhost/UrbanAccomodation/';
 
 /*
 |--------------------------------------------------------------------------
